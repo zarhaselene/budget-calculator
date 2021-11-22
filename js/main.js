a7c5cb61df489f1b2b1b3688a5ba5b166b2c3646
Zarha Buske <zarha.buske@medieinstitutet.se>
2021-11-22 10:03:56 +0100

    reset font fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-document.querySelector("button").addEventListener("click", calculateBudget)
-const incomeList = [];
-const expenseList = [];
+document.querySelector(".add-btn").addEventListener("click", calculateBudget)
+
+let incomeList = [];
+let expenseList = [];
 
 function calculateBudget(e) {
 	e.preventDefault();
@@ -8,25 +9,33 @@ function calculateBudget(e) {
 	let incomeSum = 0;
 	const operators = document.querySelector("#operators");
 	const description = document.querySelector("#description").value;
-	const value = document.querySelector("#value").value;
-	if (operators.value == "+") {
+	const value = document.querySelector("#value").value * 1;
+	// clears the input fields when "Add" is clicked
+	document.querySelector("#description").value = "";
+	document.querySelector("#value").value = "";
+
+	// if the user adds an income
+	if (operators.value === "+") {
 		document.querySelector(".incomeList").innerHTML += `<li class="income-list"> ${description}  ${value} </li>`
-		incomeList.push(value)
-	} else if (operators.value == "-") {
+		incomeList.push(value)		
+		
+	// if the user adds an expense
+	} else if (operators.value === "-") {
 		document.querySelector(".expenseList").innerHTML += `<li class="expense-list"> ${description} ${value} </li>`
 		expenseList.push(value)
 	}
 	incomeList.map((e) => {
-		incomeSum += parseFloat(e)
+		incomeSum += parseFloat(e) //converts strings to a floating-point number
 	})
 	expenseList.map((e) => {
 		costSum += parseFloat(e)
 	})
 	document.querySelector(".totalSum").textContent = incomeSum - costSum;
 }
+
+let clearButton = document.querySelector("#clear");
+clearButton.addEventListener("click", clear);
 // Function to reload and clear the page
 function clear() {
 	location.reload();
 }
-let clearButton = document.querySelector("#clear");
-clearButton.addEventListener("click", clear);
